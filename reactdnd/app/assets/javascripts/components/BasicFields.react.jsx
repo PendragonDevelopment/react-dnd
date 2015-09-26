d7e892f070f30d697d79ce7440317767a401f233
Jordan Burke <me@jordankburke.com>
2015-09-26 11:55:34 -0400

    Updated state on BasicFields

--- reactdnd/app/assets/javascripts/components/BasicFields.react.jsx
+++ reactdnd/app/assets/javascripts/components/BasicFields.react.jsx
@@ -1,10 +1,17 @@
 var BasicFields = React.createClass({
+  getInitialState: function() {
+    return {
+            name: this.props.character.name,
+            step: 1
+           }
+  },
+
   render: function() {
     return (
       <div className="basic-fields">
         <div className="form-group">
           <label>Name</label>
-          <input type="text" value={this.props.character.name} className="form-control" />
+          <input type="text" value={this.state.name} className="form-control" onChange={this.handleChange} />
         </div>
 
         <button className="btn btn-default" onClick={this.nextStep}>Next Step</button>
@@ -13,8 +20,11 @@ var BasicFields = React.createClass({
   },
 
   nextStep: function() {
-    var step = 1;
-    CharacterSheetActions.nextPage(step);
+    CharacterSheetActions.nextPage(this.state.step);
+  },
+
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
   }
 
 });
